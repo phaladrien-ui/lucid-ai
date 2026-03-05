91c0174ebd77ea2de9f1e93fcac59936a1a4d9b2
phaladrien-ui <votre-email@gmail.com>
2026-03-05 00:02:16 +0000

    Remove an unnecessary button

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { memo } from "react";
 import { useWindowSize } from "usehooks-ts";
 import { SidebarToggle } from "@/components/sidebar-toggle";
 import { Button } from "@/components/ui/button";
-import { PlusIcon, VercelIcon } from "./icons";
+import { PlusIcon } from "./icons";
 import { useSidebar } from "./ui/sidebar";
 import { VisibilitySelector, type VisibilityType } from "./visibility-selector";
 
@@ -50,7 +49,8 @@ function PureChatHeader({
         />
       )}
 
-      <Button
+      {/* Bouton Vercel temporairement désactivé 
+<Button
         asChild
         className="order-3 hidden bg-zinc-900 px-2 text-zinc-50 hover:bg-zinc-800 md:ml-auto md:flex md:h-fit dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-200"
       >
@@ -63,6 +63,7 @@ function PureChatHeader({
           Deploy with Vercel
         </Link>
       </Button>
+*/}
     </header>
   );
 }
